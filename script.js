312001b96e885dbf86eabe61cc16d831f8b6a3a1
Khushi Srivastava <workkhushisrivastava@gmail.com>
2026-04-27 15:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // This is the boilerplate code given for you
 // You can modify this code
 // Product data
+let cart = [];
+
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
@@ -14,24 +16,59 @@ const productList = document.getElementById("product-list");
 
 // Render product list
 function renderProducts() {
+  productList.innerHTML = ""; // prevent duplication
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `
+      ${product.name} - $${product.price}
+      <button class="add-to-cart-btn" data-id="${product.id}">
+        Add to Cart
+      </button>
+    `;
+    li.querySelector("button").addEventListener("click", () => {
+      addToCart(product.id);
+    });
     productList.appendChild(li);
   });
 }
 
+const cartList = document.getElementById("cart-list");
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  cartList.innerHTML = "";
+  cart.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      ${item.name} - $${item.price}
+      <button>Remove</button>
+    `;
+    li.querySelector("button").addEventListener("click", () => {
+      removeFromCart(index);
+    });
+    cartList.appendChild(li);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+  const product = products.find((p) => p.id === productId);
+  cart.push(product);
+  renderCart();
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(index) {
+  cart.splice(index, 1);
+  renderCart();
+}
 
 // Clear cart
-function clearCart() {}
+const clearCartBtn = document.getElementById("clear-cart-btn");
+function clearCart() {
+  cart = [];
+  renderCart();
+}
+clearCartBtn.addEventListener("click", clearCart);
 
 // Initial render
 renderProducts();
